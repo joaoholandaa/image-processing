4d7ca85300080b33a2b68916ede0927a7aeeb71c
Joao Holanda <joaocmdt505@gmail.com>
2023-10-13 20:58:54 -0300

    mudança de pixel

---
+++ coordenadas-pixels/mudancaPixel.py
@@ -0,0 +1,10 @@
+import cv2
+
+imagem = cv2.imread('hamilton.jpg')
+
+for y in range(0, imagem.shape[0]):
+  for x in range(0, imagem.shape[1]):
+    imagem[y, x] = (255,0,0)
+
+cv2.imshow("Imagem modificada", imagem)
+cv2.waitKey(0)
